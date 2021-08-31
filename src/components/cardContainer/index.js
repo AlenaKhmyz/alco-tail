13fc1f933e45c5c312608fcff71dec5d70246c5b
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-08-31 16:09:50 +0300

    fix: fixed cardContainer

--- src/components/cardContainer/index.js
+++ src/components/cardContainer/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import axios from 'axios'
+
 import '../../scss/components/_cardContainer.scss'
 import CardItem from '../cardItem'
 
 const CardContainer = (props) => {
+    const result = () =>  axios.get(
+        `http://localhost:3004/card`
+    ).then( (response) => { console.log(response) } )
+
     return (
         <div className="container">
             <span>{props.name}</span>
             <CardItem />
-            <button className="container__button">+</button>
+            <button className="container__button" onClick={() => { result()}}>+</button>
         </div>
     );
 }
