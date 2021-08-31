a4cc6a91e96e007bd2c29029391325457ba9f4d2
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-08-31 17:00:44 +0300

    fix: fixed component cardContainer

--- src/components/cardContainer/index.js
+++ src/components/cardContainer/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios'
 
 import '../../scss/components/_cardContainer.scss'
 import CardItem from '../cardItem'
 
 const CardContainer = (props) => {
-    const result = () =>  axios.get(
-        `http://localhost:3004/card`
-    ).then( (response) => { console.log(response) } )
+    const [cards, setCards] = useState([])
+    
+
+    const result = async () =>  {
+        try {
+            const response = await axios.get(`http://localhost:3004/card`)
+            console.log(response)
+
+            setCards(response.data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
     return (
         <div className="container">
             <span>{props.name}</span>
             <CardItem />
-            <button className="container__button" onClick={() => { result()}}>+</button>
+            <button className="container__button" onClick={result}> + </button>
+            {cards.map(({ _id, title, author, ingredients: {body}, video: {name}}) => {
+                return <div key={ _id}>{title} {author} {body} {name}</div>
+            })}
         </div>
     );
 }
