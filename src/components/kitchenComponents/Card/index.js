a879c92f1523cc94323079ab8c130900022a6a0c
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-09-13 02:23:00 +0300

    fix: fixed a component card to the kitchen

--- src/components/kitchenComponents/Card/index.js
+++ src/components/kitchenComponents/Card/index.js
@@ -1,16 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
-const MenuDishes = (props) => {
+const MenuDishes = (title, description, ingredients) => {
 
+    const [dishes, setDishes] = useState([])
+
+    const getDishes = async () => {
+       const response = await axios.get(`http://localhost:3004/dishes`)
+       setDishes(response.data)
+    } 
+
+    useEffect(() => {
+        getDishes()
+    }, [])
+
+    const createDishes= async () => {
+       const response =  await axios.post(`http://localhost:3004/dishes`, {
+            title: 'dish',
+            ingredients: [
+                {
+                    body: "some ingredient"
+                
+                }
+            ]
+        })
+
+        setDishes([...dishes, response.data])
+    }
     return (
         <div className="dish"> 
-            <h3 className ="dish__title">Dish name</h3>
-            <p className="dish__description">blablablablabla</p>
+            <h3 className ="dish__title">{title}</h3>
+            <p className="dish__description">{description}</p>
             <ul className="dish__ingredients">
-                <li className="dish__ingredients__element">apple</li>
-                <li className="dish__ingredients__element">chicken</li>
+                {ingredients.map(({name}) => {
+                    return(
+                        <li className="dish__ingredients__element">{name}</li>
+                    )
+                })}
             </ul>
+            <button className="dish__button" onClick={createDishes}> + </button>
         </div>
     )
 }
