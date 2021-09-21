0a3f7d3bbff8c5af74ea20bb3f690ee3d6ba09b5
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-09-21 03:10:46 +0300

    added form in constructor

--- src/pages/kitchenPages/Constructor/index.js
+++ src/pages/kitchenPages/Constructor/index.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
@@ -22,10 +21,24 @@ function ConstructorPage() {
     // }
     
     return (
-        <div>
-            dlfl
+        <div className="consructor">
+            <div className="constructor__container">
+                <h3 className="constructor__container__title">Dish name</h3>
+                <input className="constructor__container__name" placeholder="*Enter the name of the dish"></input>
+                <button className="constructor__container__button">+</button>
+                <form className="constructor__container__upload-container" method="POST" action="send">
+                <img className="constructor__container__upload-image" src="upload.svg"></img>
+                    <div>
+                        <input className="constructor__container__file-input" type="file" name="file" multiple></input>
+                        <label for="file-input">Выберите файл</label>
+                        <span>или перетащите его сюда</span>
+                    </div>
+                </form>
+                <textarea className="constructor__container__comments" placeholder="*Leave a comment"></textarea>
+            </div>
+            <button className="constructor__button">Save</button>
         </div> 
     )
 }
 
-export default ConstructorPage
+export default ConstructorPage  
