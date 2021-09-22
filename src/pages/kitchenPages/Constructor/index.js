d379c21270c16b811445011932d89e5eb67fdfe4
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-09-22 03:18:08 +0300

    added dropDown menu in Constructor

--- src/pages/kitchenPages/Constructor/index.js
+++ src/pages/kitchenPages/Constructor/index.js
@@ -2,6 +2,35 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
 function ConstructorPage() {
+   const ingredients = ['pepperoni','pineapple', 'cheese', 'mushrooms']
+
+   const[showDropDown, setShowDropDown] = useState(false)
+    const [list, setList] = useState(false)
+    const [finalCount, setFinalCount] = useState(0)
+    const [count, setCount] = useState(0)
+
+    const onShowDropDown = () => {
+        setShowDropDown(!showDropDown)
+    }
+
+    const onAddCount = () => {
+        setCount(count + 1)
+        
+    }
+
+    const onDeleteCount = () => {
+        if(count > 0) {
+            setCount(count - 1) 
+            
+        } else {
+            setCount(0)
+        }
+
+    }
+
+    const onShowItem = () => {
+        setList(!list)
+    }
     
     // const createDishes= async () => {
     //    const response =  await axios.post(`http://localhost:3004/dishes`, {
@@ -25,7 +54,31 @@ function ConstructorPage() {
             <div className="constructor__container">
                 <h3 className="constructor__container__title">Dish name</h3>
                 <input className="constructor__container__name" placeholder="*Enter the name of the dish"></input>
-                <button className="constructor__container__button">+</button>
+                <button className="constructor__container__name" onClick={onShowDropDown}> + </button>
+                {showDropDown && <div>
+                    { 
+                        list ? (
+                            <div> 
+                            <button onClick = {onShowItem}>Close ▲</button>
+                                <ul>
+                                    {ingredients.map( item => <li><span>{item}</span></li>)}
+                                </ul>
+                                <div>{finalCount}</div>
+                            </div>
+                           )
+                           :
+                           <button onClick = {onShowItem} style = {{width: '300px'}}>Ingredients ▼</button>
+                           
+                    }
+                     <span className="constructor__container__count">
+                        <button className="constructor__container__count__delete" onClick={onDeleteCount}>-</button>
+                        <span className="constructor__container__count__state">{count}</span>
+                        <button className="constructor__container__count__add" onClick ={onAddCount}>+</button>
+                    </span>
+                </div>}
+                
+               
+
                 <form className="constructor__container__upload-container" method="POST" action="send">
                 <img className="constructor__container__upload-image" src="upload.svg"></img>
                     <div>
